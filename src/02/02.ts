1b2c1d442c2cc02c5fec2124822706a610a54dc4
Vit Onischenko <slonik0775@gmail.com>
2022-07-20 19:05:52 +0300

    Unit test + 02 - JS/TS for students - объекты

---
+++ src/02/02.ts
@@ -0,0 +1,43 @@
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+type Technologies = {
+    id: number
+    title: string
+
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<Technologies>
+}
+
+
+const student = {
+    id: 1,
+    name: 'Vitaliy',
+    age: 28,
+    isActive: false,
+    address: {
+        streetTitle: 'Surganova 2',
+        city: {
+            title: 'Minsk',
+            countryTitle: 'Belarus'
+        }
+    },
+    technologies: [
+        {id: 1, title: 'HTML'},
+        {id: 2, title: 'JS'},
+        {id: 3, title: 'React'}
+    ]
+};
+
