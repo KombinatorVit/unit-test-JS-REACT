49c5ac82bb0b0fdd5b855b9c2daef65dd75247e9
Vit Onischenko <slonik0775@gmail.com>
2022-08-18 15:25:49 +0300

    08 - JS/TS for students - Ассоциативный массив JavaScript ание

--- src/08/01/08_01.ts
+++ src/08/01/08_01.ts
@@ -11,15 +11,17 @@ export const users: UsersType = {
     '1': {id: 1, name: 'Katya'},
 };
 
-export let usersArray = [{id: 101, name: 'Dimych'},
+export let usersArray = [
+    {id: 101, name: 'Dimych'},
     {id: 3232312, name: 'Natasha'},
     {id: 1212, name: 'Valera'},
-    {id: 1, name: 'Katya'},];
+    {id: 1, name: 'Katya'},
+];
 
 
 let user = { id: 100500, name: 'Igor'}
 
-
+users[user.id] = user
 
 
 
