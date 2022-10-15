ec99b4c1c3b4082b96b64ff884fec6549c5f85e5
Vit Onischenko <slonik0775@gmail.com>
2022-10-15 22:55:29 +0300

    10 - JS/TS for students - immutability, имутабельность  Start lessons + пишу тесты

---
+++ src/10/10_01.test.ts
@@ -0,0 +1,52 @@
+import {makeHairstyle, moveUser, UserWithLaptopType} from './10_01';
+
+export type UserType = {
+    name: string
+    hair: number
+    address: { city: string, house: number }
+}
+
+
+
+test('reference type test', () => {
+    let user: UserType = {
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        }
+    };
+
+    const awesomeUser = makeHairstyle(user, 2);
+
+
+    expect(user.hair).toBe(32);
+    expect(awesomeUser.hair).toBe(16);
+    expect(awesomeUser.address).toBe(user.address);
+
+});
+
+
+test('change address', () => {
+    let user: UserWithLaptopType = {
+        name: 'Dimych',
+        hair: 32,
+        address: {
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'ZenBook'
+        }
+    };
+
+    const movedUser = moveUser(user, 'Kiev');
+
+
+    expect(user).not.toBe(movedUser);
+    expect(user.address).not.toBe(movedUser.address);
+    expect(user.laptop).toBe(movedUser.laptop);
+    expect(movedUser.address.city).toBe('Kiev');
+
+})
--- src/10/10_01.ts
+++ src/10/10_01.ts
@@ -1,5 +1,27 @@
 export type UserType = {
     name: string
     hair: number
-    address: {title: string}
+    address: {city: string, house: number}
+}
+
+export type LaptopType = {
+    title: string
+}
+
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+
+export function makeHairstyle(u: UserType, power: number) {
+    const copy = {...u,
+        hair : u.hair / power
+    };
+    return copy;
+}
+
+export function moveUser(u: UserWithLaptopType, newCity: string){
+    const copy2 = {...u, address: {...u.address, city: newCity}
+    };
+    return copy2;
 }
