b7fd10dd4562c686f004b9430bbbba28ac5f8923
Vit Onischenko <slonik0775@gmail.com>
2022-10-16 21:39:17 +0300

    код useState Archakov

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {NavLink, Route, Routes} from 'react-router-dom';
 
 function App() {
-    const names = ['Dim', 'Sveta', 'Katya', 'Victor', 'Ignat'];
-    const users = [{name: 'Dim'}, {name: 'Sveta'}, {name: 'Katya'}, {name: 'Victor'}, {name: 'Ignat'}];
-    const liElement = names.map((n, index) => <li key={index}> {n}</li>);
-    const liUsers = users.map((u, index) => <li key={index}> {u.name}</li>);
+    // let numbers = [1, 555, 3];
+    const [numbers, setNumbers] = React.useState([1, 555, 3]);
+
+    const addNumber = () => {
+        const randNumber = Math.round(Math.random() * 10);
+        const newArr = [...numbers, randNumber];
+        setNumbers(newArr);
+    };
+
+    setInterval(() => {
+        console.log(1);
+    }, 500);
+
     return (
         <div className="App">
-
-         <NavLink to={'/'}>main</NavLink>-----
-         <NavLink to={'/login'}>login</NavLink>-----
-         <NavLink to={'/profile'}>profile</NavLink>----
-         <NavLink to={'/profile/setting'}>profile</NavLink>
-<Routes>
-    <Route path={'/*'} element={<div>404</div>}/>
-    <Route path={'/'} element={<div>main</div>}/>
-    <Route path={'login'} element={<div>login</div>}/>
-    <Route path={'/profile'} element={<div>profile</div>}/>
-    <Route path={'/profile/setting'} element={<div>setting</div>}/>
-
-
-</Routes>
+            <ul>
+                {numbers.map((num, index) => (
+                    <li key={index}>{num}</li>
+                ))}
+            </ul>
+            <button onClick={addNumber}>Новое число</button>
         </div>
     );
 }
